c83568cbd6383dc79640756b0561c31a813f1b63
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-08 16:05:01 +0700

    set nextjs ci/cd

--- infrastructure/packer/scripts/basic-website.sh
+++ infrastructure/packer/scripts/basic-website.sh
@@ -7,10 +7,11 @@ echo $PRIVATE_KEY | base64 --decode >/home/ec2-user/.ssh/private_key.id_ed25519
 chmod 400 /home/ec2-user/.ssh/private_key.id_ed25519
 
 ssh-keyscan github.com >>/home/ec2-user/.ssh/known_hosts
-eval $(ssh-agent -s)
+eval $(ssh-agent)
 
 echo "Adding private key"
-yes "" | ssh-add /home/ec2-user/.ssh/private_key.id_ed25519
+ssh-add /home/ec2-user/.ssh/private_key.id_ed25519
+echo -e "Host *\nUseKeychain yes" >> /home/ec2-user/.ssh/config
 
 echo "Cloning repo"
 cd /home/ec2-user/
