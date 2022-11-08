f6a9281e79140915bc1ce3018011ce743da71beb
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-08 15:19:50 +0700

    set nextjs ci/cd

--- infrastructure/packer/scripts/basic-website.sh
+++ infrastructure/packer/scripts/basic-website.sh
@@ -7,8 +7,10 @@ echo $PRIVATE_KEY | base64 --decode > /home/ec2-user/.ssh/private_key.id_ed25519
 
 ssh-keyscan github.com >>/home/ec2-user/.ssh/known_hosts
 eval $(ssh-agent)
-ssh-agent bash -c \
-  'ssh-add /home/ec2-user/.ssh/private_key.id_ed25519; git clone git@github.com:AriaHealth/wallet.git'
+ssh-add /home/ec2-user/.ssh/private_key.id_ed25519
+
+cd /home/ec2-user/
+git clone git@github.com:AriaHealth/wallet.git
 
 systemctl start httpd
 systemctl enable httpd
