f8d5dcb14ef02fbbb3ea0a284a1ca50dec57f39e
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-08 16:11:32 +0700

    set nextjs ci/cd

--- infrastructure/packer/scripts/basic-website.sh
+++ infrastructure/packer/scripts/basic-website.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+
+
+curl --silent --location https://rpm.nodesource.com/setup_14.x | bash -
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+
+
 yum update -y
-yum install -y httpd git
+yum install -y httpd git nodejs yarn
 
 echo $PRIVATE_KEY | base64 --decode >/home/ec2-user/.ssh/private_key.id_ed25519
 chmod 400 /home/ec2-user/.ssh/private_key.id_ed25519
@@ -9,6 +17,7 @@ chmod 400 /home/ec2-user/.ssh/private_key.id_ed25519
 ssh-keyscan github.com >>/home/ec2-user/.ssh/known_hosts
 eval $(ssh-agent)
 
+# TODO: solve the problem for private repositories
 echo "Adding private key"
 ssh-add /home/ec2-user/.ssh/private_key.id_ed25519
 echo -e "Host *\nUseKeychain yes" >> /home/ec2-user/.ssh/config
@@ -16,6 +25,10 @@ echo -e "Host *\nUseKeychain yes" >> /home/ec2-user/.ssh/config
 echo "Cloning repo"
 cd /home/ec2-user/
 git clone git@github.com:AriaHealth/wallet.git
+
+echo "Installing project"
+cd /home/ec2-user/wallet
+yarn
 
 systemctl start httpd
 systemctl enable httpd
