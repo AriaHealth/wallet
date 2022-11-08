0348bc99caddda3586ec051d4a17fa78b5f91198
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-08 14:47:49 +0700

    set nextjs ci/cd

--- infrastructure/packer/scripts/basic-website.sh
+++ infrastructure/packer/scripts/basic-website.sh
@@ -3,14 +3,12 @@
 yum update -y
 yum install -y httpd
 
-cd
-
-echo "$PRIVATE_KEY" > PRIVATE_KEY.txt
+echo $PRIVATE_KEY | base64 --decode > /home/ec2-user/.ssh/private_key.id_ed25519
 
 ssh-keyscan github.com >>/home/ec2-user/.ssh/known_hosts
 eval $(ssh-agent)
 ssh-agent bash -c \
-  'ssh-add /home/ec2-user/.ssh/[$PRIVATE_KEY]; git clone git@github.com:AriaHealth/wallet.git'
+  'ssh-add /home/ec2-user/.ssh/private_key.id_ed25519; git clone git@github.com:AriaHealth/wallet.git'
 
 systemctl start httpd
 systemctl enable httpd
