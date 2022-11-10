64da51edaa75411145d55a8526088b1e5be67734
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-10 17:34:11 +0700

    setup ci cd

--- infrastructure/packer/scripts/build-nextjs.sh
+++ infrastructure/packer/scripts/build-nextjs.sh
@@ -18,9 +18,9 @@ yarn global add pm2
 echo "Setting up environment variables"
 echo -e "[default]\naws_access_key_id=${AWS_ACCESS_KEY_ID}\naws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >$WORKDIR/.aws/credentials
 echo -e "[default]\nregion=eu-west-1\noutput=json" >$WORKDIR/.aws/config
-echo -e "export AWS_BUCKET=${AWS_BUCKET}\n" >> $WORKDIR/.bash_profile
-echo -e "export GITHUB_SHA=${GITHUB_SHA}\n" >> $WORKDIR/.bash_profile
-echo -e "export PATH=$PATH:/usr/local/bin/pm2\n" >> $WORKDIR/.bashrc
+echo -e "export AWS_BUCKET=${AWS_BUCKET}\n" >>$WORKDIR/.bash_profile
+echo -e "export GITHUB_SHA=${GITHUB_SHA}\n" >>$WORKDIR/.bash_profile
+echo -e "export PATH=$PATH:/usr/local/bin/pm2\n" >>$WORKDIR/.bashrc
 
 source $WORKDIR/.bash_profile
 source $WORKDIR/.bashrc
@@ -31,9 +31,9 @@ unzip $GITHUB_SHA.zip -d $WORKDIR/service && cd $WORKDIR/service
 cd src && yarn && yarn build
 
 echo "Running the application on pm2"
-pm2 startup systemd
-pm2 start "yarn start" --name application
-pm2 save
+/usr/local/bin/pm2 startup systemd
+/usr/local/bin/pm2 start "yarn start" --name application
+/usr/local/bin/pm2 save
 
 echo "Setting up nginx"
 rm -f /etc/nginx/conf.d/*
