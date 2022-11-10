c1e07a9c963d597fa688b23a3bab088e5de7145d
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-10 17:04:27 +0700

    setup ci cd

--- infrastructure/packer/scripts/build-nextjs.sh
+++ infrastructure/packer/scripts/build-nextjs.sh
@@ -23,6 +23,7 @@ echo -e "export GITHUB_SHA=${GITHUB_SHA}\n" >> $WORKDIR/.bash_profile
 echo -e "export PATH=$PATH:/usr/local/bin/pm2\n" >> $WORKDIR/.bashrc
 
 source $WORKDIR/.bash_profile
+source $WORKDIR/.bashrc
 
 echo "Setting up the code"
 aws s3 cp s3://${AWS_BUCKET}/$GITHUB_SHA.zip $WORKDIR
